46bffebca72fb00177e45b46a2af5a9f58d6dfeb
KateRomanova <kate.romanova@gmail.com>
2024-04-23 14:57:34 +0300

    добавлены utils.py но что-то не так с sorted

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from src.class_headhunter_api import HeadHunterApi
 from src.class_vacancies_hh import VacanciesHH
 from src.class_work_with_file import SaveJson
+from src.utils import filter_by_salary, filter_by_keyword
 
 
 # Функция для взаимодействия с пользователем
@@ -30,40 +31,12 @@ def user_interaction():
             requirements=vacancy['snippet']['requirement'])
         for vacancy in file_worker.get_vacancy()['items']]
 
+    filtered_vacancies = filter_by_keyword(vacancies, filter_words)
+    ranged_vacancies = filter_by_salary(filtered_vacancies, salary_range)
+    sorted_vacancies = sorted(ranged_vacancies, key=__gt__)
+    top_ranged_vacancies = sorted_vacancies[:top_n]
+    print(top_ranged_vacancies)
 
-def filter_by_keyword(vacancies, filter_words):
-    """ Фильтрует вакансии по ключевым словам"""
-
-    filtered_vacancies = set()
-
-    for item in filter_words:
-        for vacancy in vacancies:
-            if vacancy.description and (item in vacancy.name or item in vacancy.description):
-                filtered_vacancies.add(vacancy)
-    return filtered_vacancies
-
-
-filtered_vacancies = filter_by_keyword(vacancies, filter_words)
-
-
-def filter_by_salary(filtered_vacancies, salary_range):
-    """Фильтрует вакансии по диапазону зарплат"""
-
-    salary_list = salary_range.split()
-    salary_from_wish = int(salary_list[0])
-    salary_to_wish = int(salary_list[2])
-    ranged_vacancies = []
-
-    for vacancy in filtered_vacancies:
-        if (not vacancy.salary_from or not vacancy.salary_to or vacancy.salary_from <= salary_from_wish
-                <= vacancy.salary_to and vacancy.salary_from <= salary_to_wish <= vacancy.salary_to):
-            ranged_vacancies.append(vacancy)
-    return ranged_vacancies
-
-
-filter_by_salary.__gt__ #тут я пыталась вставить функцию для сравнения зарплат, но что-то не то получается
-
-# top_ranged_vacancies = ranged_vacancies[:top_n]
 
 if __name__ == "__main__":
     user_interaction()
